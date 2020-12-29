28ca5b08747d544a34ace26885851ad4e8d89c69
Chuck Rakaczky <cjr105@case.edu>
2020-12-29 01:04:38 -0500

    added content loaded alerts

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $("#aboutMe-para").load("/content/about.html");
+    $("#aboutMe-para").load("/content/about.html", function(responseTxt, statusTxt, xhr) {
+        if (statusTxt == "success")
+            alert("External content loaded successfully!");
+        if (statusTxt == "error")
+            alert("Error: " + xhr.status + ": " + xhr.statusText);
+    });
 });
