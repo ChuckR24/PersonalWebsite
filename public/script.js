37f4c6edeb5da06734e0dfa7714cc062707f13ee
Chuck Rakaczky <cjr105@case.edu>
2020-12-29 01:57:12 -0500

    added backToTop button and scrollbar

--- public/script.js
+++ public/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
     $("#aboutMe-para").load("content/about.html");
 });
+
+window.onscroll = function()
+{
+    var distFromTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if(distFromTop >= 100)
+    {
+        document.getElementById('return-to-top-btn').style.visibility="visible";
+    }else
+    {
+        document.getElementById('return-to-top-btn').style.visibility="hidden";
+    }
+};
