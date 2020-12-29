c42d3760fdb9d561038a0b33b6abf343a525e8bd
Chuck Rakaczky <cjr105@case.edu>
2020-12-29 01:14:25 -0500

    made content folder public

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#aboutMe-para").load("/content/about.html", function(responseTxt, statusTxt, xhr) {
+    $("#aboutMe-para").load("content/about.html", function(responseTxt, statusTxt, xhr) {
         if (statusTxt == "success")
             alert("External content loaded successfully!");
         if (statusTxt == "error")
